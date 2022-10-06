36c93ea7a7916e21169b4726b5cefc99ba193d5b
Oleg9119 <kotlyarov198433@gmail.com>
2022-10-06 11:26:09 +0300

    верстка2

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,49 +1,100 @@
 <template>
-  <div class="container mx-auto px-15">
-    <ul class="border rounded grid grid-cols-3 auto-rows-fr">
-      <li>
-        <div>
-          <img
-            class="w-full h-full object-cover"
-            :src="
-              'https://covers.openlibrary.org/b/isbn/' + testIsbn + '-M.jpg'
-            "
-            alt="Обложка"
-          />
+    <div class="container mx-auto border rounded p-6 font-sans">
+        <div class="flex flex-col items-start mb-3">
+            <button class="mb-3 px-3 h-10 transition bg-indigo-500 hover:bg-indigo-700 rounded text-white">Сортировать</button>
+            <div class="flex mb-3"><input type="text" name="bookname" id="bookname" class="mr-2 block px-3 py-2 bg-white border rounded-md text-sm shadow-sm focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500" placeholder="Название книги" /><button class="px-3 h-10 transition bg-indigo-500 hover:bg-indigo-700 rounded text-white">Загрузить</button></div>
+            <button class="mb-3 px-3 h-10 transition bg-indigo-500 hover:bg-indigo-700 rounded text-white">Выгрузить в Excel</button>
         </div>
-        <div>
-          <p>Название</p>
-          <p>Авторы</p>
-        </div>
-      </li>
-    </ul>
-  </div>
+        <ul class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-x-3 gap-y-3">
+            <li class="border rounded p-2 flex flex-col justify-between">
+                <div class="mb-2">
+                    <img class="w-full h-full object-cover" :src="'https://covers.openlibrary.org/b/isbn/' + testIsbn1 + '-M.jpg'" alt="Обложка" />
+                </div>
+                <div>
+                    <p class="text-lg font-bold italic leading-tight">Название</p>
+                    <p class="text-base font-medium leading-tight">Авторы</p>
+                </div>
+            </li>
+            <li class="border rounded p-2 flex flex-col justify-between">
+                <div class="mb-2">
+                    <img class="w-full h-full object-cover" :src="'https://covers.openlibrary.org/b/isbn/' + testIsbn2 + '-M.jpg'" alt="Обложка" />
+                </div>
+                <div>
+                    <p class="text-lg font-bold italic leading-tight">Название</p>
+                    <p class="text-base font-medium leading-tight">Авторы</p>
+                </div>
+            </li>
+            <li class="border rounded p-2 flex flex-col justify-between">
+                <div class="mb-2">
+                    <img class="w-full h-full object-cover" :src="'https://covers.openlibrary.org/b/isbn/' + testIsbn3 + '-M.jpg'" alt="Обложка" />
+                </div>
+                <div>
+                    <p class="text-lg font-bold italic leading-tight">Название</p>
+                    <p class="text-base font-medium leading-tight">Авторы</p>
+                </div>
+            </li>
+            <li class="border rounded p-2 flex flex-col justify-between">
+                <div class="mb-2">
+                    <img class="w-full h-full object-cover" :src="'https://covers.openlibrary.org/b/isbn/' + testIsbn4 + '-M.jpg'" alt="Обложка" />
+                </div>
+                <div>
+                    <p class="text-lg font-bold italic leading-tight">Название</p>
+                    <p class="text-base font-medium leading-tight">Авторы</p>
+                </div>
+            </li>
+            <li class="border rounded p-2 flex flex-col justify-between">
+                <div class="mb-2">
+                    <img class="w-full h-full object-cover" :src="'https://covers.openlibrary.org/b/isbn/' + testIsbn5 + '-M.jpg'" alt="Обложка" />
+                </div>
+                <div>
+                    <p class="text-lg font-bold italic leading-tight">Название</p>
+                    <p class="text-base font-medium leading-tight">Авторы</p>
+                </div>
+            </li>
+            <li class="border rounded p-2 flex flex-col justify-between">
+                <div class="mb-2">
+                    <img class="w-full h-full object-cover" :src="'https://covers.openlibrary.org/b/isbn/' + testIsbn6 + '-M.jpg'" alt="Обложка" />
+                </div>
+                <div>
+                    <p class="text-lg font-bold italic leading-tight">Название</p>
+                    <p class="text-base font-medium leading-tight">Авторы</p>
+                </div>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
 // import HelloWorld from "./components/HelloWorld.vue";
-const axios = require("axios").default;
-console.log("axios", axios);
+const axios = require('axios').default;
+console.log('axios', axios);
 
 export default {
-  data() {
-    return { testIsbn: "9780375822070" };
-  },
-  name: "App",
-  components: {},
-  mounted() {
-    axios.get("https://openlibrary.org/search.json?q=mr+fox");
-  },
+    data() {
+        return {
+            testIsbn1: '9780375822070',
+            testIsbn2: '9780413154408',
+            testIsbn3: '9780142414545',
+            testIsbn4: '9781847515889',
+            testIsbn5: '0517606054',
+            testIsbn6: '9788375063646',
+        };
+    },
+    name: 'App',
+    components: {},
+    mounted() {
+        axios.get('https://openlibrary.org/search.json?q=mr+fox');
+    },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
